c55cee0b59ba630576d13e27eb4888d4afa3ac4c
about-code <about-code@users.noreply.github.com>
2019-09-28 09:06:03 +0200

    fix(cli): CLI argument handling and defaults
    BREAKING CHANGE: No `--baseDir` and `--outDir` default values anymore. Asks for explicit values.
    BREAKING CHANGE: Now stops if `--baseDir` and `--outDir` resolve to same directory to prevent overriding sources. Can be ignored with `--force` flag.
    BREAKING CHANGE: CLI options now take precedence over config file options. CLI opts used together with config file opts caused config file opts to become effective.

--- bin/index.js
+++ bin/index.js
@@ -6,6 +6,7 @@ const path = require("path");
 const proc = require("process");
 const glossarify = require("../lib/glossarify");
 const confSchema = require("../conf.schema.json").properties;
+const messages = require("../lib/messages");
 
 const CWD = proc.cwd();
 const {version} = require("../package.json");
@@ -16,35 +17,25 @@ console.log(
 └──────────────────────────┘
 `)
 
-// CLI
-const parameters = Object.assign(
+const opts = Object.assign(
     {
         "config": { type: "string",  alias: "c", default: ""   , description: "Path to config file, e.g. './glossarify-md.conf.json'."},
         "help":   { type: "boolean", alias: "h", default: false, description: "Show this help."}
     },
     confSchema
 );
-const cliOpts = buildOpts(parameters);
-const args = minimist(proc.argv.slice(2), cliOpts);
 
-if (args.help || proc.argv.length === 2) {
-    console.log("Options:\n");
-    console.log(
-        Object
-            .keys(parameters)
-            .filter(key => key !== "dev")
-            .map(key => {
-                const {alias, type, description, default:_default} = parameters[key];
-                return `--${key}, --${alias} (${type})\n  ${description}\n  Default: ${JSON.stringify(_default)}\n\n`;
-            })
-            .join("")
-    );
-    process.exit(0);
-}
-
-// Read file opts
+// CLI
+const defaults = minimist([], buildOpts(opts));
+const cliArgs  = minimist(proc.argv.slice(2));
 let conf = {};
-const confPath = args.config;
+
+// --help (or no args at all)
+if (cliArgs.help || proc.argv.length === 2) {
+    printHelp(opts);
+}
+// --config
+const confPath = cliArgs.config;
 let confDir = CWD;
 if (confPath) {
     try {
@@ -56,13 +47,53 @@ if (confPath) {
     }
 }
 
-// Merge CLI opts with file opts
-conf = Object.assign(args, conf);
+// command-line arguments must take precedence over config file and defaults
+conf = Object.assign(defaults, conf, cliArgs);
 conf.baseDir = path.resolve(confDir, conf.baseDir);
 conf.outDir  = path.resolve(conf.baseDir, conf.outDir);
-console.log(`-> Reading from: ${conf.baseDir}`);
-console.log(`-> Writing to:   ${conf.outDir}
-`);
+validateConfig(conf);
 
+// Run
 glossarify.glossarify(conf);
 
+// Helpers
+function printHelp(parameters) {
+    console.log("Options:\n");
+    console.log(
+        Object
+            .keys(parameters)
+            .filter(key => key !== "dev")
+            .sort((a, b) => a.localeCompare(b, "en"))
+            .map(key => {
+                const {alias, type, description, default:_default} = parameters[key];
+                return `--${key}${alias ? ', --' + alias : ''} (${type})\n  ${description}\n  Default: ${JSON.stringify(_default)}\n\n`;
+            })
+            .join("")
+    );
+    process.exit(0);
+}
+
+function validateConfig(conf) {
+
+    if (conf.baseDir === "") {
+        console.log(messages.NO_BASEDIR);
+        console.log('ABORTED.\n');
+        proc.exit(0);
+    }
+
+    if (conf.outDir === "") {
+        console.log(messages.NO_OUTDIR);
+        console.log('ABORTED.\n');
+        proc.exit(0);
+    }
+
+    console.log(`☛ Reading from: ${conf.baseDir}`);
+    console.log(`☛ Writing to:   ${conf.outDir}
+    `);
+
+    if (conf.outDir === conf.baseDir && !conf.force) {
+        console.log(messages.OUTDIR_IS_BASEDIR)
+        console.log('ABORTED.\n');
+        proc.exit(0);
+    }
+}
