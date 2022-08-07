57a9d20bc65e810fc41152cce01a44113be9ea82
about-code <6525873+about-code@users.noreply.github.com>
2022-08-07 18:18:22 +0200

    Refactor: Use node:assert to simplify JS tools testing.

--- test/path-tools.spec.js
+++ test/path-tools.spec.js
@@ -1,27 +1,20 @@
+import { strictEqual } from "node:assert";
 import { relativeFromTo, toSystemSlash } from "../lib/path/tools.js";
-const errors = [];
 
 (function testWhenFromAndToInDifferentBranch() {
     const from = "/home/user/foo/bar/baz.md";
     const to   = "/home/user/lorem/ipsum/dolor.md";
     const expected = toSystemSlash("../../lorem/ipsum/dolor.md");
-
     const actual = toSystemSlash(relativeFromTo(from, to));
-    if (actual !== expected) {
-        errors.push(new Error(`Expected is "${expected}" but actual is "${actual}"`));
-    }
-
+    strictEqual(actual, expected);
 })();
 
 (function testWhenFromIsInSubdirectoryDepth1() {
     const from = "/home/user/foo/baz.md";
     const to   = "/home/user/dolor.md";
     const expected = toSystemSlash("../dolor.md");
-
     const actual = toSystemSlash(relativeFromTo(from, to));
-    if (actual !== expected) {
-        errors.push(new Error(`Expected is "${expected}" but actual is "${actual}"`));
-    }
+    strictEqual(actual, expected);
 
 })();
 
@@ -29,11 +22,8 @@ const errors = [];
     const from = "/home/user/foo/bar/baz.md";
     const to   = "/home/user/dolor.md";
     const expected = toSystemSlash("../../dolor.md");
-
     const actual = toSystemSlash(relativeFromTo(from, to));
-    if (actual !== expected) {
-        errors.push(new Error(`Expected is "${expected}" but actual is "${actual}"`));
-    }
+    strictEqual(actual, expected);
 
 })();
 
@@ -41,23 +31,16 @@ const errors = [];
     const from = "/home/user/baz.md";
     const to   = "/home/user/lorem/ipsum/dolor.md";
     const expected = toSystemSlash("./lorem/ipsum/dolor.md");
-
     const actual = toSystemSlash(relativeFromTo(from, to));
-    if (actual !== expected) {
-        errors.push(new Error(`Expected is "${expected}" but actual is "${actual}"`));
-    }
-
+    strictEqual(actual, expected);
 })();
 
 (function testWhenFromAndToAreSiblings() {
     const from = "/home/user/foo.md";
     const to   = "/home/user/bar.md";
     const expected = toSystemSlash("./bar.md");
-
     const actual = toSystemSlash(relativeFromTo(from, to));
-    if (actual !== expected) {
-        errors.push(new Error(`Expected is "${expected}" but actual is "${actual}"`));
-    }
+    strictEqual(actual, expected);
 
 })();
 
@@ -65,16 +48,6 @@ const errors = [];
     const from = "/home/user/lorem/ipsum/dolor.md";
     const to   = "/home/user/lorem/ipsum/dolor.md";
     const expected = toSystemSlash("./");
-
     const actual = toSystemSlash(relativeFromTo(from, to));
-    if (actual !== expected) {
-        errors.push(new Error(`Expected is "${expected}" but actual is "${actual}"`));
-    }
-
+    strictEqual(actual, expected);
 })();
-
-if (errors.length > 0) {
-    console.log(errors);
-    console.log("Tests failed with errors.");
-    process.exit(1);
-}
