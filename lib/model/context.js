about-code/glossarify-md

commit 74744a3ede184805a6064e41c2cf6a7a4ab97f9c

feat: Ambiguous term definition priority (#269)
New option `linking.sortAlternatives`. More see [Docs](https://github.com/about-code/glossarify-md/blob/master/doc/ambiguities.md).

* chore(refactor): Plug-in factories add function scope that runs once instead of once per file.
* chore(refactor): Improve performance by implementing
'linking by ID' and term-based auto linking in the
same tree-traversal. This keeps the number of
traversals by the linkifier at 2, again. The second
traversal will now be the new traversal for revisiting
term occurrences after having collected statistics
on how many terms of particular glossaries have been
mentioned and using those statistics to priortize
links for terms with multiple definitions.
* chore(docs): Update docs

lib/model/context.js
@@ -45,6 +45,10 @@ class Context {
                 ,headingDepths: arrayToMap(conf.linking.headingDepths)
                 ,limitByTermOrigin: arrayToMap(conf.linking.limitByTermOrigin)
                 ,pathRewrites: reverseMultiValueMap(conf.linking.pathRewrites)
+                ,sortAlternatives: {
+                    by: "glossary-filename"
+                    ,...conf.linking.sortAlternatives
+                }
             }
         };
 

commit 5e37defeb97b0e9649612cac95695000232c59fb

Fix some issues introduced with #269
* Fix perSectionDepth value range
* Fix perSectionDepth not being initialize with documented default.

lib/model/context.js
@@ -57,6 +57,10 @@ class Context {
         if (Math.abs(altLinks) > 95) {
             conf_.linking.limitByAlternatives = Math.sign(altLinks) * 95;
         }
+        const sortAlternatives = conf_.linking.sortAlternatives;
+        if (sortAlternatives.by === "glossary-ref-count") {
+            conf_.linking.sortAlternatives = { perSectionDepth: 2, ...sortAlternatives };
+        }
         if (conf.generateFiles.listOfFigures) {
             conf_.generateFiles.listOfFigures = { class: "figure", title: "Figures", ...conf.generateFiles.listOfFigures };
             conf_.generateFiles.listOf.push(conf.generateFiles.listOfFigures);
@@ -68,6 +72,7 @@ class Context {
         if (conf_.unified.rcPath) {
             conf_.unified.rcPath = toForwardSlash(path.resolve(conf.baseDir, conf.unified.rcPath));
         }
+
     }
 
     resolvePath(relativePath) {